56656e592f1b0f5bd7634711ffd3635088366168
William Ramsey <wramsey@gmail.com>
2018-03-11 16:41:46 -0700

    Add react-router-config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();
+  console.log(matchRoutes(Routes, req.path));
   res.send(renderer(req, store));
 });
 
