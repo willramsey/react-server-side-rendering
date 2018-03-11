3d31d157145e0e960f7f135902cb30333160ecbd
William Ramsey <wramsey@gmail.com>
2018-03-10 20:21:09 -0800

    Create client bundle.js and fetch it from browser

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm finally home</div>;
+  return (
+    <div>
+      <div>I'm finally home</div>
+      <button onClick={() => console.log('You clicked me!')} >Click me!</button>
+    </div>
+  );
 };
 
 export default Home;
