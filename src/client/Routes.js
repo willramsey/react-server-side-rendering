618adb71a23488ebf2f4208cd36dcbfa05bbe533
William Ramsey <wramsey@gmail.com>
2018-03-11 14:26:27 -0700

    Create UsersList and render on browser

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import UsersList from './components/UsersList';
 
 const Sample = () => 'Sample';
 export default () => {
   return (
     <div>
       <Route exact path="/" component={Home} />
+      <Route path="/users" component={UsersList} />
     </div>
   );
 };
---
+++ src/client/components/UsersList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UsersList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => {
+      return (
+        <li key={user.id}>{user.name}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        List of Users:
+        <ul>
+          {this.renderUsers()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users }) {
+  return { users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
