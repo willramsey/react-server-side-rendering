f8f16652b55f7fcf06793808eafc84012d2d10c4
William Ramsey <wramsey@gmail.com>
2018-03-10 20:35:38 -0800

    Hydrate app

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,5 @@
-console.log('Client working');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
