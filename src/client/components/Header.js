f90b8f8ecb4693ab088f8819e4dab087e2929110
William Ramsey <wramsey@gmail.com>
2018-03-14 01:03:26 -0700

    Customize header component

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -4,9 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Header = ({ auth }) => {
   console.log('My auth status is', auth);
+  const button = auth ? (
+    <a href="/api/logout">Sign out</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
   return (
     <div>
       <Link to="/">Home</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {button}
+      </div>
     </div>
   );
 };
