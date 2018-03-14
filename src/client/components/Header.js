b96915c82599028b77cc851f3d5302a2709dd42a
William Ramsey <wramsey@gmail.com>
2018-03-14 00:51:03 -0700

    Connect header and fetch auth status

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const Header = ({ auth }) => {
+  console.log('My auth status is', auth);
   return (
     <div>
       <Link to="/">Home</Link>
     </div>
   );
 };
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import users from './users';
+import auth from './auth';
 
-export default combineReducers({ users });
+export default combineReducers({ users, auth });
