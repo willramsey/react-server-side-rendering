7d8d3fa3a57ec057d9ac9302b132584bb2fb8044
William Ramsey <wramsey@gmail.com>
2018-03-14 00:29:43 -0700

    Add App and Header components

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import App from './App';
 import Home from './pages/Home';
 import UsersList from './pages/UsersList';
 
 export default [
   {
-    ...Home,
-    path: '/',
-    exact: true
-  },
-  {
-    ...UsersList,
-    path: '/users'
+    ...App,
+    routes: [
+      {
+        ...Home,
+        path: '/',
+        exact: true
+      },
+      {
+        ...UsersList,
+        path: '/users'
+      }
+    ]
   }
 ];
+
